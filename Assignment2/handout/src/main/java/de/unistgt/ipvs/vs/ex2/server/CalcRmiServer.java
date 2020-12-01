031fc4c2515c40e7f0b2aac397a36a3ef28982fb
ZackCHZhang <zch3183505@gmail.com>
2020-12-01 14:55:00 +0100

    ex02 part2 registry twice

--- Assignment2/handout/src/main/java/de/unistgt/ipvs/vs/ex2/server/CalcRmiServer.java
+++ Assignment2/handout/src/main/java/de/unistgt/ipvs/vs/ex2/server/CalcRmiServer.java
@@ -1,10 +1,14 @@
 package de.unistgt.ipvs.vs.ex2.server;
 
+import de.unistgt.ipvs.vs.ex2.common.ICalculation;
 import de.unistgt.ipvs.vs.ex2.common.ICalculationFactory;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,7 +24,13 @@ public class CalcRmiServer extends Thread {
 	public CalcRmiServer(String regHost, String objName) {
 		this.regHost = regHost;
 		this.objName = objName;
-                this.url = "rmi://" + regHost + "/" + objName;
+        this.url = "rmi://" + regHost + "/" + objName;
+        try {
+			Registry registry=LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	@Override
 	public void run() {
@@ -30,6 +40,32 @@ public class CalcRmiServer extends Thread {
 		}
                 
                 //Add solution here
+//		CalculationImpl calMethode=null;
+		try {
+//			System.setProperty("java.rmi.server.codebase", ICalculationFactory.class.getProtectionDomain().getCodeSource().getLocation().toString());
+//
+//		    System.setProperty("java.security.policy", "/java.policy");
+//			System.setProperty("java.rmi.server.hostname",regHost);
+//			CalcRmiServer obj=new CalcRmiServer(this.regHost,this.objName);
+			
+//			System.err.println(LocateRegistry.getRegistry(Registry.REGISTRY_PORT));
+			
+			ICalculationFactory fac=new CalculationImplFactory(url);
+			
+			java.rmi.Naming.bind(this.url, fac);
+			System.out.println("Methode Naming");
+//			System.err.println(LocateRegistry.getRegistry(Registry.REGISTRY_PORT));
+//			System.err.println(Naming.list(url));
+//			for(String s: Naming.list(url)) {
+//				System.err.println(s+"\n");
+//			}
+			
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+			e.getMessage();
+		}
+		
+		
 	}
 
         public void stopServer(){
